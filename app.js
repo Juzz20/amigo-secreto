320fb2dcee32fd2b374714e218ec6649413933fa
Justin <justinyovera@gmail.com>
2025-08-11 15:56:21 -0500

    avance del js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo (){
+    let lista = document.getElementById("amigo").value;
+    if(lista == ""){
+        alert("Por favor, inserte un nombre")
+    } else{
+        amigos.push(lista);
+    }
+    
+    console.log(lista);
+    console.log(amigos);
+    limpiar();
+}
+
+function limpiar (){
+    document.getElementById("amigo").value = "";
+}
